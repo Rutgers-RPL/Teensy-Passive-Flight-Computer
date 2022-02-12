52f184110a266b81ee2cfcb96411a259f3733eec
Carlton Wu <carltonwu@nbp-32-100.nbp.ruw.rutgers.edu>
2022-02-11 19:51:18 -0500

    added packets

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,33 @@
 #include <BMI088.h>
 #include <SdFat.h>
 
+typedef struct realPacket {
+  int magic;
+  float time;
+  float latitude;
+  float longitude;
+  float altitude;
+  float accx;
+  float accy;
+  float accz;
+  float avelx;
+  float avely;
+  float avelz;
+  float temp;
+  float pressure;
+};
+
 /* accel object */
 Bmi088Accel accel(Wire,0x18);
 /* gyro object */
 Bmi088Gyro gyro(Wire,0x68);
+
+SdFs sd;
+FsFile file;
+
+unsigned long offset = 0;
+unsigned long previousLog = 0;
+unsigned long previousTime = 0;
 
 void setup() {
   int status;
@@ -25,29 +48,41 @@ void setup() {
     Serial.println(status);
     while (1) {}
   }
+
+  if (!sd.begin(SdioConfig(FIFO_SDIO))) {
+      Serial.println("SD Begin Failed");
+  }
+  Serial.println("\nFIFO SDIO mode.");
+
+  Serial.println("Starting ...");
+  file = sd.open("datalog.txt", O_RDWR | O_CREAT | O_AT_END);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long currentLog = micros();
+
   /* read the accel */
   accel.readSensor();
   /* read the gyro */
   gyro.readSensor();
   /* print the data */
-  Serial.print(accel.getAccelX_mss());
-  Serial.print("\t");
-  Serial.print(accel.getAccelY_mss());
-  Serial.print("\t");
-  Serial.print(accel.getAccelZ_mss());
-  Serial.print("\t");
-  Serial.print(gyro.getGyroX_rads());
-  Serial.print("\t");
-  Serial.print(gyro.getGyroY_rads());
-  Serial.print("\t");
-  Serial.print(gyro.getGyroZ_rads());
-  Serial.print("\t");
-  Serial.print(accel.getTemperature_C());
-  Serial.print("\n");
-  /* delay to help with printing */
-  delay(20);
+
+  realPacket data = {0xBEEF00D, (currentLog-offset), 0.00, 0.00, 1000.0, accel.getAccelX_mss(), accel.getAccelY_mss(), accel.getAccelZ_mss(),
+                      gyro.getGyroX_rads(), gyro.getGyroY_rads(), gyro.getGyroZ_rads(), accel.getTemperature_C(), 1000.0};
+
+  if (file) {
+    file.write((const uint8_t *)&data, sizeof(data));
+    Serial.write((const uint8_t *)&data, sizeof(data)));
+  } else {
+    Serial.println("Error opening datalog.txt");
+  }
+
+  if (Serial.available()) {
+    if (!file.close()) {
+      Serial.println("Error");
+    }
+    Serial.println("Stopped!");
+    while (true);
+  }
+  previousLog = currentLog;
 }
