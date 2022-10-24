7b5bc3cd66fea331a4938551a4dc4f40faee7f40
shivammpat <shivammpat@gmail.com>
2022-10-24 17:47:05 -0400

    more Matrix changes

--- src/Matrix.h
+++ src/Matrix.h
@@ -18,23 +18,23 @@ using namespace std;
 
 class Matrix
 {
-private:
-    size_t n_rows;
-    size_t n_columns;
-    
-public:
-    float** matrix; 
-    Matrix();
-    Matrix(size_t r, size_t c);
-    Matrix(float** arg);
-    ~Matrix(void);
-    Matrix getTranspose();
-    float get(size_t r, size_t c);
-    static Matrix identity(size_t n);
-    Matrix operator * (const Matrix& m);
-    Matrix operator * (const int& m);
-    Matrix operator * (const float& m);
-    Matrix operator + (const Matrix& m);
+    private:
+        size_t n_rows;
+        size_t n_columns;
+        
+    public:
+        float** matrix; 
+        Matrix();
+        Matrix(size_t r, size_t c);
+        Matrix(float** arg);
+        ~Matrix(void);
+        Matrix getTranspose();
+        float get(size_t r, size_t c);
+        static Matrix identity(size_t n);
+        Matrix operator * (const Matrix& m);
+        Matrix operator * (const int& m);
+        Matrix operator * (const float& m);
+        Matrix operator + (const Matrix& m);
     
 };
 
